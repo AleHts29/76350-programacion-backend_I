5902f5c63d20182f49aa6d1d4e3a3d4fcd6ec391
Alejandro Huertas <alejo29h@gmail.com>
2025-05-12 20:42:28 -0300

    clase00

---
+++ clase_00/index.js
@@ -0,0 +1,55 @@
+// console.log("Hola");
+// console.warn("Warn");
+// console.error("error: esto es un error");
+
+
+// console.log(10);
+// console.log(true);
+
+let products = "valor"
+console.log(products);
+
+products = 12
+console.log(products);
+// let num = [1, 2, 3, 4, 5]
+// console.log(num);
+
+// const obj = { a: 12, b: 13, c: 14 }
+// console.log(obj);
+
+
+
+
+// 2da parte
+let name = 'Anita';
+let number = 12;
+
+
+console.log(typeof name); // ?
+console.log(typeof number); // ?
+
+const obj1 = {
+    name: "Anita",
+    last_name: "Villalba"
+}
+console.log(typeof obj1); // ? <- object
+
+const myArray = [2, 3, "Hola", true, { a: 12 }]
+console.log(myArray);
+console.log(typeof myArray); // ? <- object
+
+const obj2 = obj1;
+console.log(obj2); // ?
+
+obj2.name = "Guillermo";
+console.log("obj2: ", obj2); // ?
+
+
+console.log("obj1: ", obj1); // ?
+
+
+// todo: Como lo resuelvo ??
+
+
+
+
